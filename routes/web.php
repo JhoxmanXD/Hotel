2b7f76d90aae8626272fa312fc749b9d796dcc33
JhoxmanV <93340770+JhoxmanXD@users.noreply.github.com>
2025-12-11 08:13:53 -0500

    Add new routes for registration and checkout

--- routes/web.php
+++ routes/web.php
@@ -1,171 +1,63 @@
 <?php
 
-
-
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth; // Agregue esto por si acaso
 use App\Http\Controllers\EmployeeController;
-
 use App\Http\Controllers\PaisController;
-
 use App\Http\Controllers\ClientController;
-
 use App\Http\Controllers\PaymentMethodController;
-
 use App\Http\Controllers\RoomTypeController;
-
 use App\Http\Controllers\RoomController;
-
 use App\Http\Controllers\RegistrationController;
-
 use App\Http\Controllers\InvoiceController;
 
-
-
 Route::get('/', function () {
-
     return view('welcome');
-
 });
-
-
 
 Auth::routes();
 
-
-
-
-
 Route::middleware(['auth'])->group(function () {
-
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-    Route::resource('employees', EmployeeController::class);
-
-    Route::get('cambioestadoemployee', [EmployeeController::class, 'cambioestadoemployee'])->name('cambioestadoemployee');
-
-    Route::resource('clients', ClientController::class);
-
-    Route::get('cambioestadoclients', [ClientController::class, 'cambioestadoclients'])->name('cambioestadoclients');
-
-    Route::resource('paymentmethods', PaymentMethodController::class);
-
-    Route::get('cambioestadopaymentMethods', [PaymentMethodController::class, 'cambioestadopaymentMethods'])->name('cambioestadopaymentMethods');
-
-    Route::resource('roomtypes', RoomTypeController::class);
-
-    Route::get('cambioestadoroomtypes', [RoomTypeController::class, 'cambioestadoroomtypes'])->name('cambioestadoroomtypes');
-
-    Route::resource('rooms', RoomController::class);
-
-    Route::get('cambioestadorooms', [RoomController::class, 'cambioestadorooms'])->name('cambioestadorooms');
-
-    Route::resource('registrations', RegistrationController::class);
-
-    Route::get('cambioestadoregistrations', [RegistrationController::class, 'cambioestadoregistrations'])->name('cambioestadoregistrations');
-
-    Route::resource('invoices', InvoiceController::class);
-
-    Route::get('cambioestadoinvoices', [InvoiceController::class, 'cambioestadoinvoices'])->name('cambioestadoinvoices');
-
-    Route::get('/registration/{id}/calculate', [App\Http\Controllers\RegistrationController::class, 'calculateTotal']);
-
     
+    // Employees
+    Route::resource('employees', EmployeeController::class);
+    Route::get('cambioestadoemployee', [EmployeeController::class, 'cambioestadoemployee'])->name('cambioestadoemployee');
+    
+    // Clients
+    Route::resource('clients', ClientController::class);
+    Route::get('cambioestadoclients', [ClientController::class, 'cambioestadoclients'])->name('cambioestadoclients');
+    
+    // Payment Methods
+    Route::resource('paymentmethods', PaymentMethodController::class);
+    Route::get('cambioestadopaymentMethods', [PaymentMethodController::class, 'cambioestadopaymentMethods'])->name('cambioestadopaymentMethods');
+    
+    // Room Types
+    Route::resource('roomtypes', RoomTypeController::class);
+    Route::get('cambioestadoroomtypes', [RoomTypeController::class, 'cambioestadoroomtypes'])->name('cambioestadoroomtypes');
+    
+    // Rooms
+    Route::resource('rooms', RoomController::class);
+    Route::get('cambioestadorooms', [RoomController::class, 'cambioestadorooms'])->name('cambioestadorooms');
+    
+    // Registrations (Reservas)
+    Route::resource('registrations', RegistrationController::class);
+    Route::get('cambioestadoregistrations', [RegistrationController::class, 'cambioestadoregistrations'])->name('cambioestadoregistrations');
+    
+    // --- RUTAS NUEVAS DE LÓGICA DE NEGOCIO ---
+    
+    // 1. Calcular precio para la factura (Ya la tenías)
+    Route::get('/registration/{id}/calculate', [RegistrationController::class, 'calculateTotal']);
+    
+    // 2. Finalizar estancia / Checkout (ESTA ES LA NUEVA)
+    Route::post('/registrations/{id}/checkout', [RegistrationController::class, 'checkout'])->name('registrations.checkout');
 
+    // Invoices (Facturas)
+    Route::resource('invoices', InvoiceController::class);
+    Route::get('cambioestadoinvoices', [InvoiceController::class, 'cambioestadoinvoices'])->name('cambioestadoinvoices');
 });
 
-
-
-
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-// Auth::routes();
-
-
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-// Auth::routes();
-
-
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-// Route::resource('clients', ClientController::class);
-
-// Route::resource('employees', EmployeeController::class);
-
-// Route::resource('rooms', RoomController::class);
-
-// Route::resource('registrations', RegistrationController::class);
-
-// Route::get('cambioestadoemployee', [EmployeeController::class, 'cambioestadoemployee'])->name('cambioestadoemployee');
-
-
-
-
-
-// Route::get('/about', function () {
-
-// return 'Acerca de nosotros';
-
-// });
-
-
-
-// Route::get('/user/{id}', function ($id) {
-
-//     return 'ID de usuario: ' . $id;
-
-// });
-
-
-
-// Route::get('/contacto', function () {
-
-// return 'Página de contacto';
-
-// })->name('contacto');
-
-
-
-// Route::get('/user/{id}', function ($id) {
-
-//     return 'ID de usuario: ' . $id;
-
-// })->where('id', '[0-9]{3}');
-
-
-
-// Route::prefix('admin')->group(function () {
-
-//     Route::post('/', function () {
-
-//     return 'Panel de administración';
-
-// });
-
-// Route::post('/users', function () {
-
-//     return 'Lista de usuarios';
-
-// });
-
-// });
-
-
-
-
-
+// Ruta para probar errores 404 (Opcional)
 Route::get('/probar-404', function () {
-
-        abort(404);
-
+    abort(404);
 });
